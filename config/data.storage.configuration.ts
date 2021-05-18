63852ea8429f4a7d6c93e0686f9393be25c76fd7
Anndrijana <papicandrijanica98@gmail.com>
2021-05-18 18:24:50 +0200

    Serving static resources

--- config/data.storage.configuration.ts
+++ config/data.storage.configuration.ts
@@ -1,5 +1,6 @@
 export const DataStorageConfiguration = {
   destination: '../storage/images',
-  urlPrefix: '/assets/images',
-  maxSize: 3 * 1024 * 1024,
+  maxSize: 5 * 1024 * 1024,
+  urlPrefix: '/resources/images',
+  maxAge: 10 * 24 * 60 * 60 * 1000,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DataStorageConfiguration } from 'config/data.storage.configuration';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(DataStorageConfiguration.destination, {
+    prefix: DataStorageConfiguration.urlPrefix,
+    maxAge: DataStorageConfiguration.maxAge,
+    index: false,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
